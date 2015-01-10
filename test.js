f99ae47fb2cea90a5dee0f56d18a45709b887bd3
Trever Faden <treverfaden@gmail.com>
2015-01-10 11:07:24 -0500

    Working + Testable via npm test

---
+++ test.js
@@ -0,0 +1,6 @@
+var Random = require('./index.js');
+console.log('test test test');
+//exports.printMsg = function() {
+  console.log("This is a message from the demo package");
+  console.log(Random.secret());
+//};
